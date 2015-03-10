849bc7e0d27139037103a6dc0871351dd7321e81
Trace Pomplun <tracepomplun@gmail.com>
2015-03-10 14:59:49 -0600

    finalize parse, convert model to subclass PFObject

--- KingOfTheHill/Video.h
+++ KingOfTheHill/Video.h
@@ -2,30 +2,12 @@
 //  Video.h
 //  KingOfTheHill
 //
-//  Created by Gabriel Guerrero on 3/5/15.
+//  Created by Trace Pomplun on 3/10/15.
 //  Copyright (c) 2015 David Monson. All rights reserved.
 //
 
-#import <MapKit/MapKit.h>
-#import <Foundation/Foundation.h>
-#import "User.h"
-//#import "Location.h"
+#import <Parse/Parse.h>
 
-static NSString *videoAnnotationKey = @"videoAnnotation";
-static NSString *videoReferenceKey = @"video";
-
-static NSString *uniqueVideoIDKey = @"uniqueID";
-static NSString *locationKey = @"location";
-static NSString *videoOwnderKey = @"user";
-
-@interface Video : NSObject <MKAnnotation>
-
-@property (nonatomic, strong) User *videoOwner;
-@property (nonatomic, strong) NSString *uniqueVideoID;
-@property (nonatomic, copy) NSString *location;
-@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
-
-- (id)initWithVideoTitle:(NSString *)title atLocation:(CLLocationCoordinate2D)location;
-- (MKAnnotationView *)annotationView;
+@interface Video : PFObject
 
 @end
--- KingOfTheHill/Video.m
+++ KingOfTheHill/Video.m
@@ -2,30 +2,12 @@
 //  Video.m
 //  KingOfTheHill
 //
-//  Created by Gabriel Guerrero on 3/5/15.
+//  Created by Trace Pomplun on 3/10/15.
 //  Copyright (c) 2015 David Monson. All rights reserved.
 //
 
 #import "Video.h"
 
 @implementation Video
-
-- (id)initWithVideoTitle:(NSString *)title atLocation:(CLLocationCoordinate2D)location
-{
-    self = [super init];
-    if (self) {
-    
-    }
-    return self;
-}
-
-- (MKAnnotationView *)annotationView
-{
-    MKAnnotationView *customView = [[MKAnnotationView alloc] initWithAnnotation:self reuseIdentifier:videoAnnotationKey];
-    customView.enabled = YES;
-    customView.canShowCallout = YES;
-    customView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-    return customView;
-}
 
 @end

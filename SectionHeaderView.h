efd15951a3bdfc8d4160747ea3891c5606504d18
Trace Pomplun <tracepomplun@gmail.com>
2015-03-21 15:52:17 -0600

    bugs - save vote to video and query back to thumbnail

---
+++ SectionHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  SectionHeaderView.h
+//  KingOfTheHill
+//
+//  Created by Trace Pomplun on 3/20/15.
+//  Copyright (c) 2015 David Monson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SectionHeaderView : UIView
+
+@end
---
+++ SectionHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  SectionHeaderView.m
+//  KingOfTheHill
+//
+//  Created by Trace Pomplun on 3/20/15.
+//  Copyright (c) 2015 David Monson. All rights reserved.
+//
+
+#import "SectionHeaderView.h"
+
+@implementation SectionHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
